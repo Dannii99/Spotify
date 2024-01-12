5973ea0e8e64274df4174cb42609d410d6f22dd0
Danny Ospino <dannii.ospino@gmail.com>
2024-01-12 13:31:01 -0500

    Efecto banner

--- src/components/CardMini.vue
+++ src/components/CardMini.vue
@@ -1,24 +1,97 @@
 <template>
-    <div class="card">
-        <div class="icon" :style="{ backgroundImage: 'url(' + album.value.album.images[2].url + ')' }" v-if="album.value"/>
+    <div class="card cursor-pointer"  @mouseover="handleMouseOver" @mouseleave="ResetMouseOver" >
+        <!-- <div class="icon" :style="{ backgroundImage: 'url(' + album.value.album.images[2].url + ')' }" v-if="album.value"/> -->
+        <img ref="image" :src="album.value.album.images[2].url" alt="Sample Image" crossorigin="anonymous" class="icon"   @load="handleLoad" />
         <div class="content">
             <p class="text white">{{ album.value.album.name }}</p>
         </div>
     </div>
+    <!-- <div v-if="colorPalette">
+        <div v-for="(color, index) in colorPalette" :key="index" :style="{ backgroundColor: color }">
+            {{ color }}
+        </div>
+    </div> -->
 </template>
 
 <script setup lang="ts">
-    import { ref, onMounted, watch, type Ref, computed, watchEffect } from 'vue';
+    import { ref, onMounted, watch, type Ref, computed, watchEffect, provide, type SetupContext, getCurrentInstance } from 'vue';
 
+    // Obtener el contexto de setup manualmente
+    const { emit }: { emit: (event: string, ...args: any[]) => void }  = getCurrentInstance()!;
+
+    // variable para pasar info
     const props = defineProps<{
-        album?: any
+        album?: any,
+        hoverColor?: any,
     }>()
 
-    let album: Ref<any | null> = ref(null); //computed(() => props.user)
+    // variable almacenamiento
+    let album: Ref<any | null> = ref(null);
+
+    // Propiedad que será enviada al padre
+    const colorRef: Ref<string> = ref('');
+    const colorPalette = ref<string[] | null>(null);
+    const imageRef = ref< HTMLImageElement | null>(null);
+    
+
+    // Función para enviar el prop al padre cuando hay hover
+    const enviarPropPadre = (color: string) => {
+        colorRef.value = color;
+       // console.log(' colorRef: ',  colorRef.value);
+        props.hoverColor(colorRef.value)
+        //emit('hover-cambio', colorRef);
+    };
+
+    const handleMouseOver = () => {
+        const image:any = imageRef.value;
+        getColorPalette(image);
+    };
+
+    const ResetMouseOver = () => {
+        colorPalette.value = [`rgb(24, 40, 56)`];
+        enviarPropPadre(colorPalette.value[0])
+    };
+
+    const handleLoad = (event: Event) => {
+        const image = event.target as HTMLImageElement;
+        imageRef.value = image;
+    };
+
+    const getColorPalette = (image: HTMLImageElement) => {
+        const canvas:any = document.createElement('canvas');
+        canvas.crossOrigin = 'Anonymous';
+        const ctx:any = canvas.getContext('2d');
+        ctx.crossOrigin = 'Anonymous';
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+
+        const imageData = ctx.getImageData(0, 0, image.width, image.height).data;
+        const colors = calculateDominantColors(imageData, 5); // You can adjust the number of colors
+
+         colorPalette.value = colors.map(color => `rgb(${color[0]}, ${color[1]}, ${color[2]})`);
+         enviarPropPadre(colorPalette.value[0])
+    };
+
+    const calculateDominantColors = (imageData: Uint8ClampedArray, numColors: number): number[][] => {
+        const colors: number[][] = [];
+        for (let i = 0; i < numColors; i++) {
+            const startIndex = i * 4;
+            const color = [
+                imageData[startIndex],
+                imageData[startIndex + 1],
+                imageData[startIndex + 2],
+            ];
+            colors.push(color);
+        }
+        return colors;
+    };
+
         
     watchEffect(() => {
         album.value = computed(() => props.album);
-        console.log('album: ', album.value);
+      //  console.log('album: ', album.value);
     });
 
     onMounted(() => {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,21 @@
 <template>
     <div class="w-full h-full bg-dark px-[20px] pt-[65px] pb-5 rounded-lg">
-        <div class="banner absolute z-10 top-0 left-0 rounded-t-lg" />
+        <div class="banner absolute z-10 top-0 left-0 rounded-t-lg" :style="{ backgroundColor: colorRef }" />
         <div class="relative z-20">
             <h1 class="text-[32px] font-bold"> {{ welcome }} </h1>
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 pt-2" >
-                <CardMini :album="item" v-for="(item, index) in album.items" :key="index" />
+                <CardMini :album="item" :hoverColor="UpdateHoverColor" v-for="(item, index) in album.items" :key="index" /><!--  @hover-cambio="manejarHover" -->
             </div>
         </div>
-
-        <br><br><br><br><br><br><br><br>
-
-        <div>
-            <img ref="image" src="https://i.scdn.co/image/ab67616d0000b273f1aad814a40ec7419c234242" alt="Sample Image" crossorigin="anonymous" @load="handleLoad" @mouseover="handleMouseOver" />
-          <!--   <img ref="image" src="https://www.safecreative.org/blog/es/wp-content/uploads/2022/10/derecho-de-autor-en-imagenes.jpg" alt="Sample Image" @load="handleLoad" @mouseover="handleMouseOver" /> -->
-            <!-- <img ref="image" :src="arbol" alt="Sample Image" @load="handleLoad" @mouseover="handleMouseOver" /> -->
-            <div v-if="colorPalette">
-                <div v-for="(color, index) in colorPalette" :key="index" :style="{ backgroundColor: color }">
-                    {{ color }}
-            </div>
-        </div>
-  </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    import { ref, onMounted, type Ref } from 'vue'
+    import { ref, onMounted, type Ref, inject, computed, onBeforeUpdate, provide, watchEffect, type ComputedRef, watch } from 'vue'
     import { UserService } from '../services/api/userService'
     import { AuthService } from '../services/auth/authService'
     import { useRoute, useRouter } from 'vue-router';
-    import CardMini from './../components/CardMini.vue'
-    import arbol from "@/assets/img/bg-transparente.svg";
+    import CardMini from '@/components/CardMini.vue'
 
     // router
     const router = useRouter();
@@ -45,9 +31,14 @@
     let welcome: Ref<string> = ref('');
     const hour = new Date().getHours();
 
-    // color dominante
-    const colorPalette = ref<string[] | null>(null);
-    const imageRef = ref<HTMLImageElement | null>(null);
+    // Propiedad que recibirá del hijo
+    const colorRef:Ref< string | ComputedRef<any> | any > = ref('');
+
+    // Función que se ejecutará cuando el prop del hijo cambie
+    const UpdateHoverColor = (color: string) => {
+        colorRef.value = color;
+    };
+
 
     // saludo
     const getGreeting = (): string => {
@@ -60,58 +51,14 @@
         }
     }
 
-    const handleMouseOver = () => {
-        const image:any = imageRef.value;
-        console.log("image: ", image);
-        
-        getColorPalette(image);
-    };
+    onBeforeUpdate(() => {
 
-    const handleLoad = (event: Event) => {
-        const image = event.target as HTMLImageElement;
-        console.log("image: ", image);
-        imageRef.value = image;
-    };
-
-    const getColorPalette = (image: HTMLImageElement) => {
-        const canvas:any = document.createElement('canvas');
-        canvas.crossOrigin = 'Anonymous';
-        const ctx:any = canvas.getContext('2d');
-        ctx.crossOrigin = 'Anonymous';
-        canvas.width = image.width;
-        canvas.height = image.height;
-
-        ctx.drawImage(image, 0, 0, image.width, image.height);
-
-        const imageData = ctx.getImageData(0, 0, image.width, image.height).data;
-        const colors = calculateDominantColors(imageData, 5); // You can adjust the number of colors
-
-         colorPalette.value = colors.map(color => `rgb(${color[0]}, ${color[1]}, ${color[2]})`);
-    };
-
-    const calculateDominantColors = (imageData: Uint8ClampedArray, numColors: number): number[][] => {
-        const colors: number[][] = [];
-        for (let i = 0; i < numColors; i++) {
-            const startIndex = i * 4;
-            const color = [
-                imageData[startIndex],
-                imageData[startIndex + 1],
-                imageData[startIndex + 2],
-            ];
-            colors.push(color);
-        }
-        return colors;
-    };
-
-
+    });
+    
     onMounted(async () => {
         // const category = await service.getCategories()
         album.value = await service.getAlbum(0, 6);
-        console.log('ALBUM: ', album.value);
-        setTimeout(async () => {
-            
-        },500);
-        
+        // console.log('ALBUM: ', album.value);
         welcome.value = getGreeting();
         setInterval(() => {
             welcome.value = getGreeting();
@@ -126,5 +73,7 @@
         width: 100%;
         background-color: #535353;
         background-image: linear-gradient(#00000099 0,#121212 100%),var(--background-noise);
+        -webkit-transition: background 1s ease;
+        transition: background 1s ease;
     }
 </style>
